0cc0d4acacab179bed483d8a6b007f1c402f6dea
Luke Childs <lukechilds123@gmail.com>
2017-06-02 14:50:57 +0700

    Test streaming a response twice should fail

--- test/clone-response.js
+++ test/clone-response.js
@@ -1,6 +1,30 @@
+import http from 'http';
 import test from 'ava';
+import rfpify from 'rfpify';
+import getStream from 'get-stream';
+import createTestServer from 'create-test-server';
 import cloneResponse from '../';
+
+let s;
+const responseText = 'Hi!';
+
+test.before(async () => {
+	s = await createTestServer();
+
+	s.get('/', (req, res) => {
+		res.send(responseText);
+	});
+});
 
 test('cloneResponse is a function', t => {
 	t.is(typeof cloneResponse, 'function');
+});
+
+test('streaming a response twice should fail', async t => {
+	const response = await rfpify(http.get)(s.url + '/');
+	const firstStream = await getStream(response);
+	const secondStream = await getStream(response);
+
+	t.is(firstStream, responseText);
+	t.is(secondStream, '');
 });
