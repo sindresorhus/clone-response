36948eb2588661fedc4458150d9786985867f5e7
Luke Childs <lukechilds123@gmail.com>
2017-06-16 18:54:46 +0700

    Check a response is passed in

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const PassThrough = require('stream').PassThrough;
 const mimicResponse = require('mimic-response');
 
 const cloneResponse = response => {
+	if (!(response && response.pipe)) {
+		throw new TypeError('Parameter `response` must be a response stream.');
+	}
+
 	const clone = new PassThrough();
 	mimicResponse(response, clone);
 
